57171c89a16d662101978f189837022d805fcbad
osmanc777 <ocruz@jupi.tech>
2018-09-05 21:48:07 -0600

    cambios en menu

--- resources/views/layouts/menulateral.blade.php
+++ resources/views/layouts/menulateral.blade.php
@@ -1,19 +1,21 @@
 @section('menulateral')
     <div class="menulateral spd spi col-lg-m" ng-controller="MenulateralCtrl" ng-cloak>
         <ul>
-            <li {{{ (Request::is('/') ? 'class=active' : '') }}}>
-                <a href="{{ URL::to('/') }}" class="icovescritorio">Cuentas por cobrar</a>
-            </li>
-            <li {{{ (Request::is('crearlote') ? 'class=active' : '') }}}>
-                <a href="{{ URL::to('/crearlote') }}" class="icovtransacciones">Crear Lote</a>
-            </li>
-            <li {{{ (Request::is('transacciones') ? 'class=active' : '') }}}>
-                <a href="{{ URL::to('/transacciones') }}" class="icovtransacciones">Transacciones</a>
-            </li>
-            <li {{{ (Request::is('ajustes') ? 'class=active' : '') }}}>
-                <a href="{{ URL::to('/ajustes') }}" class="icovajustes">Ajustes</a>
+            <li>
+                <a href="{{ URL::to('/') }}" class="icovescritorio">Listado de alumnos</a>
             </li>
 
+            <li>
+                <a href="{{ URL::to('/') }}" class="icovescritorio">Asignación de cursos</a>
+            </li>
+
+            <li>
+                <a href="{{ URL::to('/') }}" class="icovescritorio">Cursos</a>
+            </li>
+
+            <li>
+                <a href="{{ URL::to('/') }}" class="icovescritorio">Ficha de alumno</a>
+            </li>
         </ul>
     </div>
 @show
